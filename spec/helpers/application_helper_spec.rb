e17b1b9bcb171f7b10eeda68216da1df067998f4
ozandlb <ozandlb@gmail.com>
2012-08-31 18:48:31 -0700

    first set of changes from baseapp to tinypost

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,7 +8,7 @@ describe ApplicationHelper do
     end
 
     it "should include the base title" do
-      full_title("foo").should =~ /^Ruby on Rails Base App/
+      full_title("foo").should =~ /^TinyPost/
     end
 
     it "should not include a bar for the home page" do
